8e63ff3c9753b1b23737d9b25eda9d79614a8e6f
Hugo LIEGEARD <hugo@biyn.media>
2020-12-07 17:19:04 +0100

    Update du 07/12
    Finalisation des Routes
    Découverte de Twig
    Mise en place du thème de base
    Création des URLs dans le menu.

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,14 +3,16 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController
 {
     /**
      * Page / Action Contact
-     * @return Response
+     * http://localhost:8000/page/contact
+     * @Route("/page/contact", name="contact_contact", methods={"GET"})
      */
-    public function contact(): Response
+    public function contact()
     {
         return new Response("<h1>Page Contact</h1>");
     }
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,17 +4,44 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController
+class DefaultController extends AbstractController
 {
     /**
      * Page / Action Accueil
-     * @Route("/", name="default_index")
+     * ex. http://localhost:8000/
+     * @Route("/", name="default_index", methods={"GET"})
      */
     public function index()
     {
-        return new Response("<h1>Page Accueil</h1>");
+        # return new Response("<h1>Page Accueil</h1>");
+
+        # Grâce à render, je vais pouvoir effectuer le rendu d'une vue.
+        return $this->render("default/index.html.twig");
+    }
+
+    /**
+     * Page / Action Catégorie
+     * Afficher les articles d'une catégorie
+     * ex. http://localhost:8000/politique, http://localhost:8000/economie, ...
+     * @Route("/{alias}", name="default_category", methods={"GET"})
+     */
+    public function category($alias)
+    {
+        return $this->render("default/category.html.twig");
+    }
+
+    /**
+     * Page / Action Article
+     * Afficher le contenu d'un article
+     * ex. http://localhost:8000/politique/covid-19-une-troisieme-vague_1.html
+     * @Route("/{category}/{alias}_{id}.html", name="default_post", methods={"GET"})
+     */
+    public function post($id, $category, $alias)
+    {
+        return $this->render("default/post.html.twig");
     }
 }
